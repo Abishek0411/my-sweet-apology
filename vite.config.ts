d19799ce8b59a5b91dacd1036f83dfbdf6ee8dd5
Abishek0411 <abishekram0411@gmail.com>
2025-08-01 21:59:20 +0530

    deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/my-sweet-apology/', // 👈 REQUIRED for GitHub Pages to resolve assets correctly
-
+  base: '/my-sweet-apology/',
   server: {
-    host: '::',
+    host: "::",
     port: 8080,
   },
-
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
